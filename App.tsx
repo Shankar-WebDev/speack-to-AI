838abf3833731e437f23b05a24be98326b0b6aa5
Sankaranarayanan <156427900+Shankar-WebDev@users.noreply.github.com>
2025-09-26 23:39:17 +0530

    01: setup file

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Text GPT</Text>
+
+      <Text style={styles.instructions}>
+        Press hold this button to record voice, Release the button to send the
+        recording, and you'll hear a response
+      </Text>
+
+      <Text style={styles.label}>Your Message:</Text>
+
+      <Text style={styles.holdButton}>Hold to Speak</Text>
+
+      <Button title="Reply last message" onPress={() => {}} />
     </View>
   );
 }
@@ -13,8 +23,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  heading: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 30,
+  },
+  instructions: {
+    textAlign: 'center',
+    marginVertical: 5,
+    fontWeight: 'bold',
+  },
+  label: {
+    marginTop: 10,
+    fontSize: 16,
+  },
+  holdButton: {
+    width: '90%',
+    padding: 30,
+    borderWidth: 3,
+    borderRadius: 10,
+    borderColor: 'lightgray',
+    textAlign: 'center',
+    marginVertical: 15,
   },
 });
